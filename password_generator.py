5925e35a444d313bc7ee0b40472346c3da3ce7fb
Aruna Chermadurai <45387204+Aruna189@users.noreply.github.com>
2022-02-25 17:56:28 +0530

    Comments have been added.

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,12 @@
-import random
-
+import random # Importing random module which selects characters randomly
+'''
+Defining all possible characters
+'''
 lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "".join([chr(ord(i)-32) for i in lower])
+upper = "".join([chr(ord(i)-32) for i in lower]) # Using list comprehension and ascii manipulation functions
 numbers = "0123456789"
 symbols =",./;:?><[]{}\|+=-_!@#$%^&*()~`"
 
 password = lower + upper + numbers + symbols
-length = int(input())
+length = int(input()) # Getting input for the length of the password
 print("".join(random.sample(password, length)))
